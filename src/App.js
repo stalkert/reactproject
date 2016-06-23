53ba2fee0c3d1ea3d3f5814ce6c7588fe4731994
Yura <taranenko.yura@gmail.com>
2016-06-23 17:57:53 +0300

    input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
 class App extends React.Component{
+	constructor(){
+		super();
+		this.state ={
+			txt:'hi 1',
+			count: 5
+		}
+		this.update = this.update.bind(this);
+	}
+	update({target}){
+		this.setState({
+			...this.state,
+			[target.getAttribute('data-param')]:target.value
+		})
+	}
 	render(){
-		return <div>Hello world!Ura </div>
+		return (
+			<div>
+			<input type="text" data-param="txt" onChange={this.update} />
+			<input type="text" data-param="count" onChange={this.update} />
+			<h1>{this.state.txt}</h1>
+			<h1>{this.state.count}</h1>
+			</div>
+			);
 
 	}
 }
+//const App = () => <h2>Hi 2</h2>
+
 export default App
